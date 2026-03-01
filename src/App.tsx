d144d1aa502b0ddb8cfe34df2dd7d3c9764fa052
EmielEDW <90001168+EmielEDW@users.noreply.github.com>
2026-03-01 12:40:36 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect, ReactNode } from "react";
+import { useState, useEffect } from "react";
+import type { ReactNode } from "react";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue, set } from "firebase/database";
 
